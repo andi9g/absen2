c88e0ce1f2a505994aab8fb9f955a5fd0ffc114e
andi9g <andirizky.bayuputra@gmail.com>
2025-02-26 00:51:20 +0700

    oke

--- app/Http/Controllers/absenC.php
+++ app/Http/Controllers/absenC.php
@@ -34,17 +34,14 @@ class absenC extends Controller
 
     $keterangan = keteranganM::get();
 
-    $absen = absenM::from("smkngunu_absensi.absen as a")
-      ->join('smkngunu_siswa.siswa as s', 'a.nisn', '=', 's.nisn')
-      ->leftJoin('smkngunu_siswa.detailsiswa as ds', 's.nisn', '=', 'ds.nisn')
-      ->leftJoin('smkngunu_siswa.kelas as k', 's.idkelas', '=', 'k.idkelas')
-      ->leftJoin('smkngunu_siswa.jurusan as j', 's.idjurusan', '=', 'j.idjurusan')
-      ->where('s.idinstansi', $idinstansi)
-      ->when($keyword, fn($q) => $q->where('ds.nama', 'like', "%$keyword%"))
-      ->when($kelas, fn($q) => $q->where('k.namakelas', $kelas))
-      ->when($jurusan, fn($q) => $q->where('j.jurusan', $jurusan))
-      ->where('a.tanggal', $tanggal)
-      ->select('a.*', 'ds.nama', 'k.namakelas', 'j.jurusan')
+    $absen = absenM::with(['siswa.detailsiswa', 'siswa.kelas', 'siswa.jurusan'])
+      ->whereHas('siswa', function ($query) use ($idinstansi, $keyword, $kelas, $jurusan) {
+        $query->where('idinstansi', $idinstansi)
+          ->when($keyword, fn($q) => $q->whereHas('detailSiswa', fn($ds) => $ds->where('nama', 'like', "%$keyword%")))
+          ->when($kelas, fn($q) => $q->whereHas('kelas', fn($k) => $k->where('namakelas', $kelas)))
+          ->when($jurusan, fn($q) => $q->whereHas('jurusan', fn($j) => $j->where('jurusan', $jurusan)));
+      })
+      ->where('tanggal', $tanggal)
       ->paginate(15);
 
     $absen->appends($request->only(["limit", "keyword", "jurusan", "kelas", "tanggal"]));
